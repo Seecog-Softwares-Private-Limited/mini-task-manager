8db62cd71b4911218b2e4b27ed6ea0a2793524d6
VinikDhariwal <vinikdhariwal04@gmail.com>
2026-07-21 17:15:29 +0530

    Fix

--- src/modules/feedbacks/feedbacks.module.ts
+++ src/modules/feedbacks/feedbacks.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { OrganizationsModule } from '../organizations/organizations.module';
 import { FeedbackEntity } from './entities/feedback.entity';
 import { FeedbacksRepository } from './repositories/feedbacks.repository';
 import { FeedbacksService } from './feedbacks.service';
 import { FeedbacksController } from './feedbacks.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FeedbackEntity])],
+  imports: [
+    TypeOrmModule.forFeature([FeedbackEntity]),
+    AuthModule,
+    OrganizationsModule,
+  ],
   controllers: [FeedbacksController],
   providers: [FeedbacksRepository, FeedbacksService],
   exports: [FeedbacksService],
